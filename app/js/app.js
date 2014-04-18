e83b66cefefd08e83131d0c9c764deb6a7c22901
John Herndon <johnlherndon@gmail.com>
2014-04-17 21:03:29 -0600

    refactoring

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,8 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/view1', {templateUrl: 'app/partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/view2', {templateUrl: 'app/partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/login', {templateUrl: 'app/partials/login.html', controller: 'MyCtrl3'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
